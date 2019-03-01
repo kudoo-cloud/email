633c1552c97227d66ae64b543d6e95acdeab4b86
Suhani Shah <suhanishah228@gmail.com>
2019-03-01 15:33:04 +0530

    Added currency to invoice email

--- src/views/emails/mjml.ts
+++ src/views/emails/mjml.ts
@@ -1,6 +1,6 @@
 import mjml2html from 'mjml';
 
-export const SubscriptionMJMLToHTMLConverter = ({ company_owner, company_name, invoice_total, subscription_url }) => mjml2html(`
+export const SubscriptionMJMLToHTMLConverter = ({ company_owner, company_name, company_currency, invoice_total, subscription_url }) => mjml2html(`
   <mjml>
     <mj-body>
       <mj-section>
@@ -17,7 +17,7 @@ export const SubscriptionMJMLToHTMLConverter = ({ company_owner, company_name, i
             color="#434856"
             font-family="'Roboto', sans-serif">
               We've noticed that you've reached the limit of our free tier licensing.<br/> <br/>
-              ${company_name} has issued Invoices of ${invoice_total} and that means you have been automatically added
+              ${company_name} has issued Invoices of ${company_currency} ${invoice_total} and that means you have been automatically added
               to our paid subscription plan. <br/> <br/>
               Please click to add your credit card details to ensure that you don't lose access to your Kudoo company.
           </mj-text>
